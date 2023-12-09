7e7db9f60701637dada372f3935039d3028fa480
Gabriel Regalado <regs0404@gmail.com>
2023-12-09 00:42:08 -0500

    working on adding monthly total and adding class to footer

--- client.js
+++ client.js
@@ -1,38 +1,37 @@
+
 function submitForm(event) {
     console.log('Submit employee');
+    // Assigning input values to variables
+    let firstName = document.querySelector('#firstNameInput').value;
+    let lastName = document.querySelector('#lastNameInput').value;
+    let idNumber = document.querySelector('#idInput').value;
+    let jobTitle = document.querySelector('#titleInput').value;
+    let annualSalary = document.querySelector('#annualSalaryInput').value;
+    
+    // Append to the body of the table
+    let employeeTable = document.querySelector('#employeeList');
 
-        // Find the input field and assign the value to our variable
-        let firstName = document.querySelector('#firstNameInput').value;
-        let lastName = document.querySelector('#lastNameInput').value;
-        let idNumber = document.querySelector('#idInput').value;
-        let jobTitle = document.querySelector('#titleInput').value;
-        let annualSalary = document.querySelector('#annualSalaryInput').value;
-
-        // Find the tbody on the page so that we can append to it
-        let employeeTable = document.querySelector('#employeeList');
-        // let lastIndex = temperatureTable.lastElementChild;
-        // let tableData = Number(lastIndex.firstElementChild.innerHTML);
-        employeeTable.innerHTML += `
-            <tr>
-                <td>${firstName}</td>
-                <td>${lastName}</td>
-                <td>${idNumber}</td>
-                <td>${jobTitle}</td>
-                <td>${annualSalary}</td>
-                <td><button onclick="deleteRow(event)>Delete</button></td>
-            </tr>
+    // Add values to table
+    employeeTable.innerHTML += `
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${idNumber}</td>
+    <td>${jobTitle}</td>
+    <td>${annualSalary}</td>
+    <td><button onclick="deleteRow(event)">Delete</button></td>
 `;
 
-let monthlySalary = annualSalary / 12;
-console.log(monthlySalary);
-
+    
+    //Reset form
+    document.querySelector("#addEmployeeForm").reset();
 };
 
+//Creating monthly total variable
 let monthlyTotal = 0;
 
 
-
+//Function to delete row with button
 function deleteRow(event) {
     console.log('delete row');
-    event.target.parentNode.remove();
+    event.target.parentNode.parentNode.remove();
 }
