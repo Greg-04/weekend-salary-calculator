a4261a302f1ddf3bc86da563f90000c00f768519
Gabriel Regalado <regs0404@gmail.com>
2023-12-08 23:49:08 -0500

    working on adding delete button and need to add total

--- client.js
+++ client.js
@@ -1,5 +1,34 @@
+function submitForm(event) {
+    console.log('Submit employee');
 
+        // Find the input field and assign the value to our variable
+        let firstName = document.querySelector('#firstNameInput').value;
+        let lastName = document.querySelector('#lastNameInput').value;
+        let idNumber = document.querySelector('#idInput').value;
+        let jobTitle = document.querySelector('#titleInput').value;
+        let annualSalary = document.querySelector('#annualSalaryInput').value;
 
+        // Find the tbody on the page so that we can append to it
+        let employeeTable = document.querySelector('#employeeList');
+        // let lastIndex = temperatureTable.lastElementChild;
+        // let tableData = Number(lastIndex.firstElementChild.innerHTML);
+        employeeTable.innerHTML += `
+            <tr>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${idNumber}</td>
+                <td>${jobTitle}</td>
+                <td>${annualSalary}</td>
+                <td><button onclick="deleteRow(event)>Delete</button></td>
+            </tr>
+`;
+
+let monthlySalary = annualSalary / 12;
+console.log(monthlySalary);
+
+};
+
+let monthlyTotal = 0;
 
 
 
